c38a513bbddf81ea59e7a6631f370a76ae48ba78
rabe69 <rabe69@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-10-21 07:56:15 +0000

    Add userfunc to parse bbCode-styled links in list and single view.
    E.g.
    [url="http://www.test.de"]Test[/url]
    
    To activate it for e.g. field uid125 in single view use:
    plugin.tx_powermailfrontend_pi1 {
      single {
         uid125 = TEXT
         uid125.field = value
         uid125.postUserFunc = user_powermailfrontendParseLinks->user_parseLinks
         uid125.postUserFunc.ATagParams = class="test"
         uid125.postUserFunc.target = _blank
      }
    }
    
    Attention: 
    This userfunc does not use typolink to generate the links.
    Only "ATagParams" and "target" parameters supported (we just use the same parameter name)
    
    Also [mail="test@test.de"]Test-Email-Address[/mail] is supported.
    But with NO encryption of the output!
    
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/powermail_frontend/trunk@53228 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ lib/user_powermailfrontend_parseLinks.php
@@ -0,0 +1,72 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2011 Alexander Grein <ag@mediaessenz.eu>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * user_powermailfrontendParseLinks() parse content and transform e.g. [url="http:://www.test.de"]Test[/url] to a real link <a href="http:://www.test.de">Test</a>
+ *
+ * @param	string		$content: The PlugIn content
+ * @param	array		$conf: The PlugIn configuration
+ * @return	The content that is displayed on the website
+ */
+
+require_once(PATH_tslib.'class.tslib_pibase.php');
+
+class user_powermailfrontendParseLinks extends tslib_pibase {
+
+	var $cObj;// The backReference to the mother cObj object set at call time
+
+	function user_parseLinks($content = '', $conf = array()) {
+
+		$target = '';
+		$aTagParams = '';
+
+		if (!empty($conf['target'])) {
+			$target = ' target="' . $conf['target'] . '"';
+		}
+
+		if (!empty($conf['target'])) {
+			$aTagParams = ' ' . $conf['ATagParams'];
+		}
+
+		if (TYPO3_MODE != 'FE') { // only in Frontend
+			return false;
+		}
+		$bbcode = array(
+			'[url=&quot;', '[/url]',
+			'[mail=&quot;', '[/mail]',
+			'&quot;]'
+		);
+		$htmlcode = array(
+			'<a' . $target . $aTagParams . ' href="', '</a>',
+			'<a href="mailto:', '</a>',
+			'">'
+		);
+        return str_replace($bbcode, $htmlcode, $content);
+	}
+}
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/powermail_frontend/lib/user_powermailfrontendParseLinks.php'])	{
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/powermail_frontend/lib/user_powermailfrontendParseLinks.php']);
+}
+?>
